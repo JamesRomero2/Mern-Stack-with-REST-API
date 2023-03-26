865600d1220b340b604a07e77d67aefaaa8f3218
unknown <haymeemotoh22@gmail.com>
2023-03-26 23:20:28 +0800

    Deployment Fixes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const {errorHandler} = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 connectDB()
 
@@ -13,6 +14,8 @@ const app = express()
 //MIDDLEWARE
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+app.use(cors())
 
 // RULES OF API
 app.use((req, res, next) => {
