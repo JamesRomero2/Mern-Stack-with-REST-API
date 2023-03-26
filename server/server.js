7851049253a28731b3680813a9cb7ae2103d6c0a
unknown <haymeemotoh22@gmail.com>
2023-03-26 22:43:28 +0800

    Deployment Fixes

--- server/server.js
+++ server/server.js
@@ -17,14 +17,14 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-// SERVE FRONT-END
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+// // SERVE FRONT-END
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-} else {
-  app.get('/', (req,res) => res.send('Please set to Production'))
-}
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+// } else {
+//   app.get('/', (req,res) => res.send('Please set to Production'))
+// }
 
 app.use(errorHandler)
 
