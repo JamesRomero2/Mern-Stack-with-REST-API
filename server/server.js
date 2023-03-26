a379393da584702a1d54cafb058ae85e11840cc8
unknown <haymeemotoh22@gmail.com>
2023-03-26 22:51:18 +0800

    Deployment Fixes

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+// RULES OF API
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
